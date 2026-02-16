924d599ac57f8eb213dcb65149b90175ed7eb006
semperfitodd <todd@bernsonfamily.com>
2026-02-16 14:52:11 -0500

    working to get frontend deployed

--- services/frontend/vite.config.ts
+++ services/frontend/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    assetsDir: 'static',
+  },
   server: {
     host: '0.0.0.0',
     port: 3000
