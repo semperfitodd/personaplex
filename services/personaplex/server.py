9f29a3fae751fb246c52e5060e45bc488d86fab6
semperfitodd <todd@bernsonfamily.com>
2026-02-18 09:42:41 -0500

    working to get frontend deployed

--- services/personaplex/server.py
+++ services/personaplex/server.py
@@ -7,15 +7,8 @@ args = ["-m", "moshi.server", "--ssl", ssl_dir, "--host", "0.0.0.0", "--port", "
 if os.environ.get("CPU_OFFLOAD", "false").lower() == "true":
     args.append("--cpu-offload")
 
-if os.environ.get("USE_FLOAT16", "false").lower() == "true":
-    args.append("--half")
-
 hf_repo = os.environ.get("HF_REPO", "")
 if hf_repo:
     args.extend(["--hf-repo", hf_repo])
-
-cfg_coef = os.environ.get("CFG_COEF", "")
-if cfg_coef:
-    args.extend(["--cfg-coef", cfg_coef])
 
 os.execvp("python3", ["python3"] + args)
